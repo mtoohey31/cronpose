0a87418b573626a8ad363a40469df7d463ecc637
Matthew Toohey <contact@mtoohey.com>
2022-03-06 23:32:54 -0500

    style: improve readability with better spacing

--- main.go
+++ main.go
@@ -18,10 +18,12 @@ func connect() *net.UnixConn {
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
 	conn, err := net.DialUnix("unix", nil, addr)
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
 	return conn
 }
 
@@ -30,10 +32,12 @@ func makeReqest(conn *net.UnixConn, method string, url string, body io.Reader, v
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
 	err = req.Write(conn)
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
 	res, err := http.ReadResponse(bufio.NewReader(conn), req)
 	if err != nil {
 		log.Fatal().Msg(err.Error())
@@ -42,20 +46,24 @@ func makeReqest(conn *net.UnixConn, method string, url string, body io.Reader, v
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
 	err = json.Unmarshal(resBody, &v)
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
 	return res.StatusCode
 }
 
 func main() {
 	conn := connect()
+
 	var containers []types.Container
 	status := makeReqest(conn, http.MethodGet, "http://localhost/containers/json?all=true&filters=%7B%22label%22%3A%5B%22com.mtoohey.cronpose%22%5D%7D", nil, &containers)
 	if status != http.StatusOK {
 		log.Error().Msgf("response status %d while listing containers", status)
 	}
+
 	c := cron.New()
 	for _, currContainer := range containers {
 		// NOTE: this is required because the loop values are passed by reference
@@ -68,9 +76,12 @@ func main() {
 		if !ok {
 			schedule = container.Labels["com.mtoohey.cronpose"]
 		}
+
 		log.Info().Msgf("detected container %v with schedule %s", container.Names, schedule)
+
 		_, err := c.AddFunc(schedule, func() {
 			log.Info().Msgf("starting container %v", container.Names)
+
 			status := makeReqest(conn, http.MethodPost, fmt.Sprintf("http://localhost/containers/%s/start", container.ID), nil, nil)
 			if status != http.StatusNoContent && status != http.StatusNotModified {
 				log.Error().Msgf("response status %d while starting container %v", status, container.Names)
